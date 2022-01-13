04c7f0ccf7f8b683f6c9164fce1456fc2d9ae853
孙振宇 <4343490@qq.com>
2022-01-13 09:36:46 +0800

    add new webpack minimizer

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,12 @@ const webpack = require('webpack')
 const pkg= require('./package.json')
 const CompressionPlugin = require('compression-webpack-plugin')
 const AutoZip = require('webpack-auto-zip')
+const TerserJSPlugin = require("terser-webpack-plugin");
 
 process.env.VUE_APP_VER = pkg.version
 process.env.VUE_APP_BUILD = new Date().toLocaleString()
+const packNameList = new Array(60).fill('').map((e, i) => ({name: `package${i}`, count: 0}))
+
 module.exports = {
     configureWebpack: config => {
         config.plugins.push(
@@ -43,5 +46,67 @@ module.exports = {
         if (process.env.NODE_ENV === 'production') {
             config.plugins.push(new AutoZip(`${pkg.name}_${pkg.version}_${process.env.VUE_APP_ENV}.zip`))
         }
+        config
+            .optimization = {
+            usedExports: true,
+            sideEffects: true,
+            concatenateModules: true,
+            minimize: true,
+            minimizer: [
+                new TerserJSPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: false,
+                }),
+            ],
+            splitChunks: {
+                chunks: 'initial',
+                // minSize: 0,
+                // maxSize: 200000,
+                minChunks: 2,
+                maxAsyncRequests: 5,
+                maxInitialRequests: 5,
+                cacheGroups: {
+                    default: false,
+                    common: {
+                        name: "chunk-common",
+                        chunks: "initial",
+                        minChunks: 2,
+                        // maxInitialRequests: 5,
+                        minSize: 0,
+                        priority: 1,
+                        reuseExistingChunk: true,
+                        enforce: true
+                    },
+                    vendors: {
+                        // name: "chunk-vendors",
+                        name(module) {
+                            // get the name. E.g. node_modules/packageName/not/this/part.js
+                            // or node_modules/packageName
+                            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1].toLowerCase()
+                            const index = packNameList.findIndex(e => e.count <= 15)
+                            if (index > -1 && packageName.indexOf('vue') === -1) {
+                                const one = packNameList[index]
+                                packNameList[index].count = packNameList[index].count + 1
+                                return one.name
+                            }
+                            // npm package names are URL-safe, but some servers don't like @ symbols
+                            return `package.${packageName.replace('@', '')}`
+                        },
+                        test: /[\\/]node_modules[\\/]/,
+                        minChunks: 2,
+                        maxSize: 1024 * 300,
+                        chunks: "initial",
+                        priority: 10,
+                        reuseExistingChunk: true,
+                        enforce: true
+                    }
+                }
+            },
+            runtimeChunk: {
+                name: 'manifest'
+            }
+        }
     }
+
 }
